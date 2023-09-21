3ce63a8a1863c6a3e06b7782ed9ce396c9d8cad8
gaouimounir <mounir.gaoui@outlook.com>
2023-09-21 16:47:19 +0200

    maj js

--- main.js
+++ main.js
@@ -8,24 +8,24 @@ var image3 = document.getElementById("image3");
 var texte3 = document.getElementById("texte3");
 
 image1.addEventListener("click", function () {
-  if (texte1.style.display === "none") {
-    texte1.style.display = "block";
+  if (texte1.style.display === "none" || texte1.style.display === "") {
+    texte1.style.display = "block" && "flex";
   } else {
     texte1.style.display = "none";
   }
 });
 
 image2.addEventListener("click", function () {
-  if (texte2.style.display === "none") {
-    texte2.style.display = "block";
+  if (texte2.style.display === "none" || texte2.style.display === "") {
+    texte2.style.display = "block" && "flex";
   } else {
     texte2.style.display = "none";
   }
 });
 
 image3.addEventListener("click", function () {
-  if (texte3.style.display === "none" && window.innerWidth < 500) {
-    texte3.style.display = "block";
+  if (texte3.style.display === "none" || texte3.style.display === "") {
+    texte3.style.display = "block" && "flex";
   } else {
     texte3.style.display = "none";
   }

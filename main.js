3957e9ebaf912a39e2d12727c2a98edbf0145365
gaouimounir <mounir.gaoui@outlook.com>
2023-09-21 16:58:53 +0200

    ajustement image

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var texte3 = document.getElementById("texte3");
 
 image1.addEventListener("click", function () {
   if (texte1.style.display === "none" || texte1.style.display === "") {
-    texte1.style.display = "block" && "flex";
+    texte1.style.display = "block";
   } else {
     texte1.style.display = "none";
   }
@@ -17,7 +17,7 @@ image1.addEventListener("click", function () {
 
 image2.addEventListener("click", function () {
   if (texte2.style.display === "none" || texte2.style.display === "") {
-    texte2.style.display = "block" && "flex";
+    texte2.style.display = "block";
   } else {
     texte2.style.display = "none";
   }
@@ -25,7 +25,7 @@ image2.addEventListener("click", function () {
 
 image3.addEventListener("click", function () {
   if (texte3.style.display === "none" || texte3.style.display === "") {
-    texte3.style.display = "block" && "flex";
+    texte3.style.display = "block";
   } else {
     texte3.style.display = "none";
   }
